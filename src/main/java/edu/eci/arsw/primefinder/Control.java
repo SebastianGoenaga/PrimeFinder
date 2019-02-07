bc978dcf56478496752811236c8310f868a457cf
Sebastian <sebastian.goenaga@hotmail.com>
2019-02-06 20:35:28 -0500

    parte2prime

--- src/main/java/edu/eci/arsw/primefinder/Control.java
+++ src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -5,40 +5,70 @@
  */
 package edu.eci.arsw.primefinder;
 
+import java.util.Scanner;
+
 /**
  *
  */
 public class Control extends Thread {
-    
-    private final static int NTHREADS = 3;
-    private final static int MAXVALUE = 30000000;
-    private final static int TMILISECONDS = 5000;
 
-    private final int NDATA = MAXVALUE / NTHREADS;
+	private final static int NTHREADS = 10;
+	private final static int MAXVALUE = 30000000;
+	private final static int TMILISECONDS = 5000;
 
-    private PrimeFinderThread pft[];
-    
-    private Control() {
-        super();
-        this.pft = new  PrimeFinderThread[NTHREADS];
+	private final int NDATA = MAXVALUE / NTHREADS;
 
-        int i;
-        for(i = 0;i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
-            pft[i] = elem;
-        }
-        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
-    }
-    
-    public static Control newControl() {
-        return new Control();
-    }
+	private PrimeFinderThread pft[];
+	private Boolean flag;
 
-    @Override
-    public void run() {
-        for(int i = 0;i < NTHREADS;i++ ) {
-            pft[i].start();
-        }
-    }
-    
+	private Control() {
+		super();
+		this.pft = new PrimeFinderThread[NTHREADS];
+		flag = true;
+
+		int i;
+		for (i = 0; i < NTHREADS - 1; i++) {
+			PrimeFinderThread elem = new PrimeFinderThread(i * NDATA, (i + 1) * NDATA, this);
+			pft[i] = elem;
+		}
+		pft[i] = new PrimeFinderThread(i * NDATA, MAXVALUE + 1, this);
+	}
+
+	public Boolean getFlag() {
+		return flag;
+	}
+
+	public static Control newControl() {
+		return new Control();
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < NTHREADS; i++) {
+			pft[i].start();
+		}
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			try {
+				Thread.sleep(1000);
+				flag = false;
+				for (PrimeFinderThread p : pft) {
+					System.out.println(p.getPrimes().size());
+				}
+				System.out.println();
+				System.out.println("Presione enter para continuar calculando primos");
+				System.out.println();
+				sc.nextLine();
+				flag = true;
+				synchronized (this) {
+					this.notifyAll();
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
 }
--- src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,45 +3,55 @@ package edu.eci.arsw.primefinder;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PrimeFinderThread extends Thread{
+public class PrimeFinderThread extends Thread {
 
-	
-	int a,b;
-	
+	int a, b;
+
 	private List<Integer> primes;
-	
-	public PrimeFinderThread(int a, int b) {
+	private Control control;
+
+	public PrimeFinderThread(int a, int b, Control control) {
 		super();
-                this.primes = new LinkedList<>();
+		this.primes = new LinkedList<>();
 		this.a = a;
 		this.b = b;
+		this.control = control;
 	}
 
-        @Override
-	public void run(){
-            for (int i= a;i < b;i++){						
-                if (isPrime(i)){
-                    primes.add(i);
-                    System.out.println(i);
-                }
-            }
+	@Override
+	public void run() {
+		for (int i = a; i < b; i++) {
+			if (!control.getFlag()) {
+				synchronized (control) {
+					try {
+						control.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			if (isPrime(i)) {
+				primes.add(i);
+			}
+		}
+
 	}
-	
+
 	boolean isPrime(int n) {
-	    boolean ans;
-            if (n > 2) { 
-                ans = n%2 != 0;
-                for(int i = 3;ans && i*i <= n; i+=2 ) {
-                    ans = n % i != 0;
-                }
-            } else {
-                ans = n == 2;
-            }
-	    return ans;
+		boolean ans;
+		if (n > 2) {
+			ans = n % 2 != 0;
+			for (int i = 3; ans && i * i <= n; i += 2) {
+				ans = n % i != 0;
+			}
+		} else {
+			ans = n == 2;
+		}
+		return ans;
 	}
 
 	public List<Integer> getPrimes() {
 		return primes;
 	}
-	
+
 }
